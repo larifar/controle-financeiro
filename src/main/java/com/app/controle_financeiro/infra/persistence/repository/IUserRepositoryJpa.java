1885255ebec0648c8e0cff03336a556c990f3985
Larissa Faria <lari.f4ria@gmail.com>
2025-07-14 11:02:43 -0300

    create UserRepositoryImpl

--- src/main/java/com/app/controle_financeiro/infra/persistence/repository/IUserRepositoryJpa.java
+++ src/main/java/com/app/controle_financeiro/infra/persistence/repository/IUserRepositoryJpa.java
@@ -4,6 +4,10 @@ import com.app.controle_financeiro.infra.persistence.entities.UserEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface IUserRepositoryJpa extends JpaRepository<UserEntity, Long>{
+    Optional<UserEntity> findByEmail(String email);
+    Optional<UserEntity> findByTelegramId(long telegramId);
 }
