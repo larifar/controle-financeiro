f76e0c9401b4fe05a6b8e4df19e5d9d0519e1471
Larissa Faria <lari.f4ria@gmail.com>
2025-07-14 15:59:33 -0300

    add swagger

--- src/main/java/com/app/controle_financeiro/infra/config/SecurityConfig.java
+++ src/main/java/com/app/controle_financeiro/infra/config/SecurityConfig.java
@@ -12,7 +12,7 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/users/**").permitAll()
+                        .requestMatchers("**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .csrf(CsrfConfigurer::disable);
